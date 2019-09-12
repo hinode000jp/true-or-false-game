e6aa52f8d9054aa48a1ef17f71817cf8e0f8324e
hinode000jp <takenoko000jp@gmail.com>
2019-09-12 17:50:29 +0900

    update

--- js/main.js
+++ js/main.js
@@ -2,6 +2,9 @@
 const titleText = document.getElementById('title');
 const questionText = document.getElementById('question');
 
+let sum;
+let correctAnser;
+
 const questions = [
     {question: '小説「ハリー・ポッター」シリーズの作者は男である。○か×か。', anser: 0},
     {question: 'ポケモンと妖怪ウォッチ、先にゲームが発売されたのは妖怪ウォッチである。○か×か。', anser: 0},
@@ -10,23 +13,20 @@ const questions = [
 ];
 onStart();
 
-function anser(a) {
-
-    if (sum < questions.length) {
-
-        if (a === questions[sum].anser) {
-            console.log('正解');
-            correctAnser++;
-        } else {
-            console.log('不正解');
-        }
-        sum++;
+function anser(ans) {
+    if (ans === questions[sum].anser) {
+        console.log('正解');
+        correctAnser++;
+    } else {
+        console.log('不正解');
     }
+    sum++;
     if (sum === questions.length) {
         alert(`${questions.length}問中${correctAnser}問正解です`);
         onStart();
+    } else {
+        writeQuestion();
     }
-    writeQuestion();
 }
 
 function onStart() {
